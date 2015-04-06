8504f07786d1251ba8152f4527649158e4b0487b
= <venkat0535@gmail.com>
2015-04-06 17:00:36 +0530

    edited application controller,devise edit view,devise new view,removed links from header,configured omniauth routes and added our user omniauth controller

---
+++ app/controllers/users/omniauth_callback_controller.rb
@@ -0,0 +1,15 @@
+class users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+	 def facebook
+	 	@user = User.find_or_create_from_auth_hash auth_hash 
+	 	if @user.persisted?
+	 		sign_in_and_redirect @user
+	 	else
+	 		session["devise.user_attributes"] = @user.attributes
+	 		redirect_to new_user_registration_url,notice: "oops something went wrong"
+	 	end
+	 end
+	 
+	 def auth_hash
+	 	request.env['omniauth.auth']
+	 end
+end
